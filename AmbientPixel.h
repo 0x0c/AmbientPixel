cbcabf030ac404c5f4439d59d125d60548e759f5
Akira Matsuda <akira.matsuda@me.com>
2015-08-06 15:11:37 +0900

    initial commit

---
+++ AmbientPixel.h
@@ -0,0 +1,31 @@
+#include <StandardCplusplus.h>
+#include <system_configuration.h>
+#include <unwind-cxx.h>
+#include <utility.h>
+#include <vector>
+#include <skInfraredCOM.h>
+
+namespace AmbientPixel
+{
+	class Pixel
+	{
+		#define spx_send_port 3
+		#define spx_receive_port 2
+		#define spx_send_broadcast 255
+		skInfraredCOM *ir_port;
+		unsigned char device_id;
+	public:
+		Pixel(unsigned char device_id) {
+			this->ir_port = new skInfraredCOM(spx_send_port, spx_receive_port);
+			this->device_id = device_id;
+		}
+
+		void send(unsigned char data, unsigned char send_to) {
+			this->ir_port->Send(send_to, data);
+		}
+
+		unsigned char receive() {
+			return this->ir_port->Recive(this->device_id);
+		}
+	};
+}
---
+++ examples/Node_send/Node_send.ino
@@ -0,0 +1,19 @@
+#include <skInfraredCOM.h>
+#include <AmbientPixel.h>
+
+AmbientPixel::Pixel pixel(0x01);
+void setup()
+{
+ 	// put your setup code here, to run once:
+ 	Serial.begin(9600);
+}
+
+void loop()
+{
+	// put your main code here, to run repeatedly:
+	int i;
+	for (i = 0x30 ; i < 0x7b ; i++) {
+		pixel.send((unsigned char)i, spx_send_broadcast);
+		delay(10);
+	}
+}
