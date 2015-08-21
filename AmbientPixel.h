946eb871f7f20391f86460ea8da516344c6c90a5
Akira Matsuda <akira.matsuda@me.com>
2015-08-22 07:05:42 +0900

    add comment

--- AmbientPixel.h
+++ AmbientPixel.h
@@ -16,11 +16,17 @@ namespace AmbientPixel
 				Broadcast = 255,
 			};
 		};
-
+		// 送信元デバイスID
+		unsigned char from;
+		// 送信先デバイスID
 		unsigned char dest;
+		// パケットID
 		unsigned char id;
+		// データ
 		unsigned char *data;
+		// データ長
 		int length;
+
 		Packet(unsigned char id, unsigned char *data, int length, unsigned char dest) {
 			this->id = id;
 			this->data = data;
@@ -37,12 +43,24 @@ namespace AmbientPixel
 	class Pixel
 	{
 	private:
+		// Masterの方向のポート
+		// このポートをたどるとMasterへたどり着く
+		char master_port;
+		// フルカラーLED制御用変数
 		Adafruit_NeoPixel led;
+		// 送信・受信ポート
 		std::vector<skInfraredCOM *> ports;
+		// ポートに対応するデバイスID
+		std::vector<unsigned char> port_device_id;
+		// パケットの受信数
 		unsigned char packet_sequence_count;
+		// デバイスID
 		unsigned char device_id;
+		// 頂点数
 		unsigned char number_of_vertex;
+		// ロック用変数
 		bool block;
+
 		void port_close() {
 			// 隣り合う受信モジュールで受信しないようにロックを掛ける
 			this->block = true;
@@ -74,20 +92,23 @@ namespace AmbientPixel
 		struct Vertex {
 			enum {
 				Triangle = 3,
-				Rectangle = 4,
-				Pentagon = 5,
-				Hexagon = 6,
+				// Rectangle = 4,
+				// Pentagon = 5,
+				// Hexagon = 6,
+				NumberOfMaximumVertex = 3,
 			};
 		};
 
 		Pixel(unsigned char device_id, unsigned char number_of_vertex) {
-			#define maximum_vertex 6
-			if (number_of_vertex <= maximum_vertex) {
+			if (number_of_vertex <= AmbientPixel::Pixel::Vertex::NumberOfMaximumVertex) {
 				for (int i = 0; i < number_of_vertex; ++i) {
 					this->ports.push_back(new skInfraredCOM(i * 2, i * 2 + 1));
 				}
 				this->packet_sequence_count = 0;
 				this->device_id = device_id;
+				if (this->device_id == 0) {
+					this->master_port = -1;
+				}
 				this->number_of_vertex = number_of_vertex;
 				this->led = = Adafruit_NeoPixel(1, 13, NEO_GRB + NEO_KHZ800);
 			}
@@ -123,8 +144,8 @@ namespace AmbientPixel
 		}
 
 		// パターンデータを伝搬させる
-		void forward(unsigned char port_no, char *data) {
-			// dataをPacketに格納し、port_noの辺からデータを送信する
+		void forward(unsigned char port_no, Packet p) {
+			// パケットをport_noの辺からデータを送信する
 		}
 
 		unsigned char packet_id() {
