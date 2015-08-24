977b9944e76f63ab9b2131a92cd321d81468e365
Akira Matsuda <akira.matsuda@me.com>
2015-08-24 18:29:48 +0900

    add firmware project(mock)

--- examples/Node_send/Node_send.ino
+++ examples/Node_send/Node_send.ino
@@ -5,7 +5,8 @@
 #include <skInfraredCOM.h>
 #include <AmbientPixel.h>
 
-AmbientPixel::Pixel pixel(0x01, AmbientPixel::Pixel::Vertex::Triangle);
+uint8_t device_id = 0x01;
+AmbientPixel::Pixel pixel(device_id, AmbientPixel::Pixel::Vertex::Triangle);
 void setup()
 {
  	// put your setup code here, to run once:
@@ -14,8 +15,11 @@ void setup()
 
 void loop()
 {
-	unsigned char data[16] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f};
-	AmbientPixel::Packet packet(pixel.packet_id(), data, 16, AmbientPixel::Packet::Dest::Broadcast);
-	pixel.send(0, packet);
+	// uint8_t data[16] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f};
+	AmbientPixel::Packet echo_packet(pixel.packet_id(), AmbientPixel::Packet::Type::Echo, NULL, 0, AmbientPixel::Packet::Dest::Broadcast);
+	
+	uint8_t port_no = 0;
+	pixel.send(port_no, echo_packet);
+	
 	delay(1000);
 }
