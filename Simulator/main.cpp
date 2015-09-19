4817844d94906c95d9a60e92c8b6e4693c85ed90
Akira Matsuda <akira.matsuda@me.com>
2015-09-19 23:33:54 +0200

    fix simulator

--- Simulator/main.cpp
+++ Simulator/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
 	// TODO:
 	// 1.Pixelの形に対応するように、VirtualPixelインスタンスを生成する。（VirtualPixelを作り、right）
 	// 2.生成したインスタンスにMasterからNWパケットを送信する。
-	// 3.ネットワーク全体の状態（Device IDが幾つになったか、LEDの状態はどうなっているか）を表示する。
+	// 3.ネットワーク全体の状態（Device IDが幾つになったか、LEDの状態はどうなっているか）を表示する。dumpメソッドをMasterのPixelで呼び出す。
 	// 4.適当にPatternパケットを流してLEDの状態を変化させる。
 	// 5.再度ネットワーク全体の状態を表示する。
 
