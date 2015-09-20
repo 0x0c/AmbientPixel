d517e1528762d82f6b00473620ce4fc0eff27030
Akira Matsuda <akira.matsuda@me.com>
2015-09-20 23:17:04 +0200

    use pods

--- App/PatternMaker/PatternMaker/Library/Konashi/KonashiUtils.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <CoreBluetooth/CoreBluetooth.h>
-
-extern NSString *NSStringFromCBCentralManagerState(CBCentralManagerState);
-extern NSString *NSStringFromCBCharacteristicProperty(CBCharacteristicProperties);
-extern NSString *NSStringFromCFUUIDRef(CFUUIDRef);
-extern CBUUID *kns_CreateUUIDFromString(NSString *, CBUUID *);
