848c5a58a7e30d65989e25ab552c6544da34b8d0
Akira Matsuda <akira.matsuda@me.com>
2015-10-11 23:12:53 +0900

    add design file

---
+++ App/PatternMaker/PatternMaker/Class/ViewController/WeatherPatternViewController/APPMWeatherPatternViewController.h
@@ -0,0 +1,13 @@
+//
+//  APPMWeatherPatternViewController.h
+//  PatternMaker
+//
+//  Created by Akira Matsuda on 10/4/15.
+//  Copyright © 2015 Akira Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APPMWeatherPatternViewController : UIViewController
+
+@end
---
+++ App/PatternMaker/PatternMaker/Class/ViewController/WeatherPatternViewController/APPMWeatherPatternViewController.m
@@ -0,0 +1,13 @@
+//
+//  APPMWeatherPatternViewController.m
+//  PatternMaker
+//
+//  Created by Akira Matsuda on 10/4/15.
+//  Copyright © 2015 Akira Matsuda. All rights reserved.
+//
+
+#import "APPMWeatherPatternViewController.h"
+
+@implementation APPMWeatherPatternViewController
+
+@end
