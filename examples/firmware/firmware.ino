0ef0c7aed2d2c0188d28696c150e81cb57a0cf75
村田 遥人 <murataharuto@murata-no-MacBook-Pro.local>
2015-10-12 19:19:09 +0900

    マスターのfirmware(テスト用)

--- examples/firmware/firmware.ino
+++ examples/firmware/firmware.ino
@@ -8,7 +8,7 @@
 
 using namespace AmbientPixel;
 
-uint8_t device_id = 0b00000000; // 0b00000000 ~ 0b00000111まで上位3bitを使用
+uint8_t device_id = 0b00000001; // 0b00000000 ~ 0b00000111まで上位3bitを使用
 Pixel pixel(AmbientPixel::Pixel::Vertex::Triangle);
 
 void setup()
@@ -22,17 +22,90 @@ void setup()
 int cnt = 0;
 void loop()
 {
+  /*
 	uint8_t data = pixel.watch(cnt);
 	if(data != 0) {
 	    pixel.receive(cnt, data);
 	}
-	cnt = (cnt + 1) % 3;
+ */
+	//cnt = (cnt + 1) % 3;
 }
 
 void serialEvent()
 {
-	// アプリからパケットを受信
 	char data = Serial.read();
-	pixel.send(0, data);
-	Serial.flush();
+  switch (data) {
+    case '-': {
+      pixel.send(0, Packet::packet_data(0b00000000, Pixel::Flag::Control, Pixel::ControlFlag::Reset));
+      pixel.send(1, Packet::packet_data(0b00000000, Pixel::Flag::Control, Pixel::ControlFlag::Reset));
+      pixel.send(2, Packet::packet_data(0b00000000, Pixel::Flag::Control, Pixel::ControlFlag::Reset));
+    }
+      break;
+    case '=': {
+      pixel.send(0, Packet::packet_data(0b00100000, Pixel::Flag::Control, Pixel::ControlFlag::Network));
+      pixel.send(1, Packet::packet_data(0b01000000, Pixel::Flag::Control, Pixel::ControlFlag::Network));
+      pixel.send(2, Packet::packet_data(0b01100000, Pixel::Flag::Control, Pixel::ControlFlag::Network));
+    }
+      break;
+
+    case 'r': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::Glow, Pixel::Color::Red));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::Glow, Pixel::Color::Red));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::Glow, Pixel::Color::Red));
+
+      //pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::Glow, Pixel::Color::Red));
+    }
+      break;
+    case 'g': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::Glow, Pixel::Color::Green));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::Glow, Pixel::Color::Green));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::Glow, Pixel::Color::Green));
+
+      //pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::Glow, Pixel::Color::Green));
+    }
+      break;
+    case 'b': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::Glow, Pixel::Color::Blue));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::Glow, Pixel::Color::Blue));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::Glow, Pixel::Color::Blue));
+
+      pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::Glow, Pixel::Color::Blue));
+    }
+      break;
+    case 'o': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::TurnOff, Pixel::Color::Off));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::TurnOff, Pixel::Color::Off));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::TurnOff, Pixel::Color::Off));
+
+      //pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::TurnOff, Pixel::Color::Off));
+    }
+      break;
+    case 'R': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::Blink, Pixel::Color::Red));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::Blink, Pixel::Color::Red));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::Blink, Pixel::Color::Red));
+
+      //pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::Blink, Pixel::Color::Red));
+    }
+      break;
+    case 'G': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::Blink, Pixel::Color::Green));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::Blink, Pixel::Color::Green));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::Blink, Pixel::Color::Green));
+
+      //pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::Blink, Pixel::Color::Green));
+    }
+      break;
+    case 'B': {
+      pixel.send(0, Packet::packet_data(0b01000000, Pixel::Flag::Blink, Pixel::Color::Blue));
+      pixel.send(0, Packet::packet_data(0b01100000, Pixel::Flag::Blink, Pixel::Color::Blue));
+      pixel.send(0, Packet::packet_data(0b10000000, Pixel::Flag::Blink, Pixel::Color::Blue));
+
+      //pixel.receive(0, Packet::packet_data(0b00100000, Pixel::Flag::Blink, Pixel::Color::Blue));
+    }
+      break;
+    default:
+      break;
+  }
+  Serial.flush();
 }
